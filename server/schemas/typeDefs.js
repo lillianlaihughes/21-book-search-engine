d56884661919ef91a5fa6ed722374fbbebed1a63
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-22 19:52:25 -0400

    updated Book.js and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,8 +33,8 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveBook(newBook: bookData!): User
-    removeBook(bookId: String!): User
+    saveBook(newBook: Book!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
