05051c09c96f0087271f32ae8a35e576675564a7
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-22 23:10:44 -0400

    fixed error in Mutation typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,7 +33,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveBook(newBook: Book!): User
+    saveBook(newBook: String!): User
     removeBook(bookId: ID!): User
   }
 `;
